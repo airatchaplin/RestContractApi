2efcbc5fc12956555afbe8d401843283e0c35b0a
Airat_Salikhov <airatchaplin@gmail.com>
2021-02-18 20:23:03 +0400

    fix bug

--- src/main/java/com/restapi/controllers/ContractController.java
+++ src/main/java/com/restapi/controllers/ContractController.java
@@ -2,20 +2,21 @@ package com.restapi.controllers;
 
 import com.restapi.model.Contract;
 import com.restapi.repository.ContractRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ContractController {
 
-    @Autowired
+    final
     ContractRepository contractRepository;
+
+    public ContractController(ContractRepository contractRepository) {
+        this.contractRepository = contractRepository;
+    }
 
     @GetMapping("/contracts")
     List<Contract> getAllContracts() {
